4094d80b7fc86908f4c91f8f3bead2d4831e462d
Meet1512 <iconic1512@gmail.com>
2025-07-04 19:49:55 +0530

    Handeled single number input

--- src/TDDStringCalculator/StringCalculator.java
+++ src/TDDStringCalculator/StringCalculator.java
@@ -5,6 +5,6 @@ public class StringCalculator {
 		if(numbers == null || numbers.isEmpty()) {
 			return 0;
 		}
-		return -1;
+		return Integer.parseInt(numbers);
 	}
 }
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -12,4 +12,11 @@ public class StringCalculatorTest {
 		int result = calculator.add("");
 		assertEquals(0,result);
 	}
+	
+	@Test
+	void singleNumberReturnItsValue() {
+		assertEquals(5,calculator.add("5"));
+		assertEquals(123,calculator.add("123"));
+		assertEquals(12345,calculator.add("12345"));
+	}
 }
